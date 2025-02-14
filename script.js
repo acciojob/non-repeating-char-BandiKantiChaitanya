53ffeaf352c342f6460ee461f898abcd767ddfec
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-14 16:13:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,11 @@ function firstNonRepeatedChar(str) {
  // Write your code here
  let res = null;
 for (let char of str) {
-    if (
-        str.indexOf(char)
-        ===
-        str.lastIndexOf(char)
-    ) {
+    if (str.indexOf(char)===str.lastIndexOf(char)) {
         res = char;
-		return res
-        break;
-    }else{
-		return 'null'
-	}
+    }
 }
+	return res
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string"); 
 alert(firstNonRepeatedChar(input)); 
